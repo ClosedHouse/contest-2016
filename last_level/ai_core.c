b680af2ecdb9a2ed5c44d56426a7489e66169833
Robin Hack <rhack@redhat.com>
2016-03-29 14:54:35 +0200

    last_level: Added last nightmare level

---
+++ last_level/ai_core.c
@@ -0,0 +1,47 @@
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/init.h>		/* Needed for the macros */
+#include <linux/timer.h>
+
+static void ai_core_periodic(unsigned long not_used);
+static void ai_core_weak_point(void);
+
+
+static struct timer_list ai_core_periodic_val;
+static char buffer[32];
+static int __init ai_core_init(void)
+{
+	printk(KERN_NOTICE "AI GlaDOS module started successfully.");
+
+	init_timer(&ai_core_periodic_val);
+	ai_core_periodic_val.expires = jiffies + 10000;
+	ai_core_periodic_val.function = ai_core_periodic;
+	ai_core_periodic_val.data = 0;
+	add_timer(&ai_core_periodic_val);
+
+	return 0;
+}
+
+static void ai_core_periodic(unsigned long not_used)
+{
+	ai_core_weak_point();
+
+	del_timer(&ai_core_periodic_val);
+	init_timer(&ai_core_periodic_val);
+
+	ai_core_periodic_val.expires = jiffies + 10000;
+	ai_core_periodic_val.function = ai_core_periodic;
+	ai_core_periodic_val.data = 0;
+	add_timer(&ai_core_periodic_val);
+
+}
+
+static void ai_core_weak_point(void)
+{
+	strcpy(buffer, "OH_424242");
+}
+
+MODULE_LICENSE("GPL");
+
+module_init(ai_core_init);
+//module_exit(madvise_swap_exit);

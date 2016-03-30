b7476508e7fcf535ffbb5cb826b78fa029f9be40
Robin Hack <hack.robin@gmail.com>
2016-03-30 19:48:38 +0200

    html/task3: Add more info to task3.

--- html/task3.php
+++ html/task3.php
@@ -1,8 +1,8 @@
 <?php
 $hints = array(
-"You can use the netcat tool for port scanning...",
-"I know nobody's able to remember all the command line switches of all the unix tools, but... <br/><strong>RTFM!</strong>",
-"Well, if you really insist on that... What about `nc -zv hostname 1-1024`?",
+"You can use the netcat tool for port scanning... nothing will be bad if you use nmap",
+"I know nobody's able to remember all the command line switches of all the unix tools, but... <br/><strong>RTGM!</strong>",
+"Well, if you really insist on that... What about `nc -zv glados 1024-12345` or `nmap glados`?",
 );
 ?>
 <h1>Level 3</h2>
@@ -12,6 +12,10 @@ as they seem to be... What if there's also some backdoor? Some narrow pipe, hidd
 in the bushes behind the fortress...</p>
 
 <p>Try to scan the ports, maybe it's listening somewhere...</p>
+
+<p>After scan you can take a look at directory:<br>
+~/contest-2016/remote/ and investigate exec.asm file.
+</p>
 
 <?php if(is_hint() && $hints.count() != 0): ?>
 

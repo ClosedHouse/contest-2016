e2ff256e65bce281a0584a6416d6af49a570f2f8
Robin Hack <hack.robin@gmail.com>
2016-04-04 20:09:08 +0200

    html/task3: More hints and more userfriendliness.

--- html/task3.php
+++ html/task3.php
@@ -1,10 +1,10 @@
 <?php
 $hints = array(
-"You can use the netcat (nc command) tool for port scanning... nothing will be bad if you use nmap.",
-"I know nobody's able to remember all the command line switches of all the unix tools, but... <br/><strong>RTFM!</strong> ;-)",
-"Well, if you really insist on that... What about `nc -zv glados 1024-12345` or `nmap glados`?",
-"You can use netcat for feeding remote network services with some stuff.",
-"You do not have to type the byte opcodes to netcat manually; if you compile an asm file, it would serve well.",
+	"Maybe you found nothing. What if backdoor is open on port higher than 1024?",
+	"You should look for `port ranges` string in nmap man page.<br>I know that range from 1-65535 is too large to scan. What about to start with 12245-12345 .",
+	"`nmap -p 12245-12345 glados`",
+	"You can use `nc` command for feeding remote backdoor with bytecode.",
+	"`cat exec | nc glados 12345`",
 );
 ?>
 <h1>Level 3</h2>
@@ -14,11 +14,16 @@ as they seem to be... What if there's also some backdoor? Some narrow pipe, hidd
 in the bushes behind the fortress...</p>
 
 <p>You vaguely remember once there was a famous backdoor that was executing bytecode
-received from an open network port. What if Glados suffers from it too? Anyway, a port
+received from an open network port. What if glados suffers from it too? Anyway, a port
 scan is always a good point to start.</p>
+
+<p>Nmap is installed to your machine. You can use it.</p>
 
 <p>After scan you may have a look at directory:<br>
 ~/contest-2016/task3/ and investigate the exec.asm file.
+File contains remote exploit which reveals some secrets to you.
+You must compile this file and send it to port which you found.<br>
+If you are really curios hacker, you should take a look at exec.asm file content.
 </p>
 
 <?php if(is_hint()): ?>

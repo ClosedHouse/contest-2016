0636a9d1121d04f9341b3081440dd93627fd06b1
Frantisek Sumsal <frantisek@sumsal.cz>
2016-04-04 15:13:07 +0200

    Show last exit code in prompt

--- conf/run-tmux.sh
+++ conf/run-tmux.sh
@@ -4,6 +4,17 @@ SESSION=OH16
 DEF_DIR=/tmp
 LINKS="links http://localhost:8080/"
 
+cat > ~/.bashrc << EOF
+if [ -f /etc/bashrc ]; then
+    . /etc/bashrc
+fi
+
+export PROMPT_COMMAND=__prompt_command
+function __prompt_command() {
+    PS1="[\$? \u@\h \W]\\\$ "
+}
+EOF
+
 cd $DEF_DIR
 tmux new-session -s "$SESSION" -n "Shell" -d 
 tmux new-window -t "$SESSION:1" -n "Instructions" $LINKS

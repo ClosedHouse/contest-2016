76664bf14d722fd9647c2021bd181c52056f67c7
Robin Hack <hack.robin@gmail.com>
2016-04-04 21:25:04 +0200

    html/task4: Fixess...

--- html/task4.php
+++ html/task4.php
@@ -2,21 +2,23 @@
 $hints = array(
 "The code used for encryption is 42.",
 "I don't know what I can advise to you now... :-(",
+"What about printf?",
+"`printf "%x\n" 42`",
 );
 ?>
 <h1>Level 4</h2>
 <p>Great! You have obtained the contents of the /secrets file (did you looked what exec.asm really do?).
 Damn! You gained code but another message is encrypted! Again!</p>
 
-<p>Do you remember code used for encryption from level 2?</p>
+<p>Do you remember code used for encryption from level 2? You will need it here.</p>
 
-<p>Use the sse.asm file and try to force the AI machine to solve the problem for you. Don't forget
-to edit the necessary data in it! Who knows what's encrypted there, it might be even the root password
+<p>You can find utils for this level in <strong>~/contest-2016/task4</strong> directory<br>
+<p>You should aim to sse.asm file which use powerful SSE instructions of your CPU and can decrypt message very fast</p>
+<p>Don't forget to edit the necessary data in it! Who knows what's encrypted there, it might be even the root password
 of the glados machine and more...</p>
 
 <p>
-Craft the sse.asm file to make it decrypt the encrypted content and send it to the AI machine.
-You find it again in <strong>~/contest-2016/task4</strong><br>
+As usual, you can use `make` command for compilation in right directory.
 </p>
 
 <?php if(is_hint()): ?>

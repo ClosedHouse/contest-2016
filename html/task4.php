a934b88b001c0b1d2a513cae24736c2949268878
Frantisek Sumsal <frantisek@sumsal.cz>
2016-04-05 10:29:07 +0200

    task4: edit text

--- html/task4.php
+++ html/task4.php
@@ -8,12 +8,12 @@ $hints = array(
 ?>
 <h1>Level 4</h2>
 <p>Great! You have obtained the contents of the /secrets file (did you looked what exec.asm really do?).
-Damn! You gained code but another message is encrypted! Again!</p>
+Damn! You gained code but there's another encrypted message! Again!</p>
 
-<p>Do you remember code used for encryption from level 2? You will need it here.</p>
+<p>Do you remember the code used for encryption in level 2? You will need it here.</p>
 
-<p>You can find utils for this level in <strong>~/contest-2016/task4</strong> directory<br>
-<p>You should aim to sse.asm file which use powerful SSE instructions of your CPU and can decrypt message very fast</p>
+<p>You can find utils for this level in <strong>~/contest-2016/task4</strong> directory.<br>
+<p>You should aim to sse.asm file which uses powerful SSE instructions of your CPU and can decrypt message very fast.</p>
 <p>Don't forget to edit the necessary data in it! Who knows what's encrypted there, it might be even the root password
 of the glados machine and more...</p>
 

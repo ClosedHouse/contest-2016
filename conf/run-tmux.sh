d6f18057a4a57a0d7b040721404cd8eb04d53fed
Robin Hack <rhack@redhat.com>
2016-04-05 08:13:22 +0200

    conf/run-tmux.sh: Fixes

--- conf/run-tmux.sh
+++ conf/run-tmux.sh
@@ -4,11 +4,12 @@ SESSION=OH16
 DEF_DIR=/tmp
 LINKS="links http://localhost:8080/"
 
-cat > ~/.bashrc << EOF
+cat > ~/.bashrc << "EOF"
 if [ -f /etc/bashrc ]; then
     . /etc/bashrc
 fi
 
+export HISTFILESIZE=0
 export PROMPT_COMMAND=__prompt_command
 function __prompt_command() {
     PS1="[\$? \u@\h \W]\\\$ "
@@ -17,6 +18,6 @@ EOF
 
 cd $DEF_DIR
 tmux new-session -s "$SESSION" -n "Shell" -d 
-tmux new-window -t "$SESSION:1" -n "Instructions" $LINKS
+tmux new-window -t "$SESSION:1" -n "Instructions" "$LINKS"
 tmux select-window -t "$SESSION:1"
 tmux -2 attach-session -t "$SESSION"
